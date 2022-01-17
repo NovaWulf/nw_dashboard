fc25c709df1201fe98fda883324785f2d0d6abe4
Nader Akhnoukh <iamnader@gmail.com>
2022-01-17 14:05:33 +0000

    update messari with default dates

--- server/app/lib/messari.rb
+++ server/app/lib/messari.rb
@@ -2,23 +2,23 @@ class Messari
   include HTTParty
   base_uri 'data.messari.io/api/v1'
 
-  DEAFULT_START_DATE = Date.new(2017,1,1)
+  DEFAULT_START_DATE = Date.new(2017, 1, 1)
 
   def initialize
     @options = { headers: { "x-messari-api-key": ENV['MESSARI_API_KEY'] } }
   end
 
-  def btc_price(start_date = DEAFULT_START_DATE)
+  def btc_price(start_date = DEFAULT_START_DATE)
     self.class.get("/assets/bitcoin/metrics/price/time-series?start=#{start_date}&end=#{DateTime.now.to_date}&interval=1d",
                    @options)
   end
 
-  def circulating_marketcap(start_date = DEAFULT_START_DATE)
+  def btc_circ_mcap(start_date = DEFAULT_START_DATE)
     self.class.get("/assets/bitcoin/metrics/mcap-circ/time-series?start=#{start_date}&end=#{DateTime.now.to_date}&interval=1d",
                    @options)
   end
 
-  def all_realized_marketcap(start_date = DEAFULT_START_DATE)
+  def btc_realized_mcap(start_date = DEFAULT_START_DATE)
     self.class.get("/assets/bitcoin/metrics/mcap-realized/time-series?start=#{start_date}&end=#{DateTime.now.to_date}&interval=1d",
                    @options)
   end
