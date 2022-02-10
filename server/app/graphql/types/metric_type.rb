44aa5be6f74f3927844460ecfd83030378fe14ec
Nader Akhnoukh <iamnader@gmail.com>
2022-02-10 13:33:10 +0000

    updated api

--- server/app/graphql/types/metric_type.rb
+++ server/app/graphql/types/metric_type.rb
@@ -1,13 +1,10 @@
 module Types
   class MetricType < Types::BaseObject
     field :id, ID, null: false
-    field :n, String, null: true
+    field :metric, String, null: true
+    field :token, String, null: true
     field :ts, Integer, null: false
     field :v, Float, null: false
-
-    def n
-      object.name
-    end
 
     def ts
       object.timestamp.to_time.to_i
