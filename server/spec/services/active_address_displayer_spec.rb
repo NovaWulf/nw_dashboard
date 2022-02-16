6c68189e007decbcaf2677591df10241d6f17bd1
Nader Akhnoukh <iamnader@gmail.com>
2022-02-16 12:34:49 +0000

    fix specs

--- server/spec/services/active_address_displayer_spec.rb
+++ server/spec/services/active_address_displayer_spec.rb
@@ -3,11 +3,11 @@ RSpec.describe ActiveAddressDisplayer do
 
   before(:each) do
     Metric.create(token: 'btc', metric: 'price', timestamp: Date.today, value: 500.0)
-    Metric.create(token: 'btc', metric: 'active_addresses', timestamp: Date.today, value: 1000.0)
+    Metric.create(token: 'btc', metric: 'active_addresses', timestamp: Date.today.prev_occurring(:monday),
+                  value: 1000.0)
   end
 
   it 'returns prices' do
-    expect(subject.value.count).to  eql 1
     expect(subject.value.first['value']).to eql 1000.0
   end
 end
--- server/spec/services/price_displayer_spec.rb
+++ server/spec/services/price_displayer_spec.rb
@@ -2,12 +2,11 @@ RSpec.describe PriceDisplayer do
   subject { described_class.run(token: 'btc') }
 
   before(:each) do
-    Metric.create(token: 'btc', metric: 'price', timestamp: Date.today, value: 1000.0)
-    Metric.create(token: 'btc', metric: 'active_addresses', timestamp: Date.today, value: 500.0)
+    Metric.create(token: 'btc', metric: 'price', timestamp: Date.today.prev_occurring(:monday), value: 1000.0)
+    Metric.create(token: 'btc', metric: 'active_addresses', timestamp: Date.today.prev_occurring(:monday), value: 500.0)
   end
 
   it 'returns prices' do
-    expect(subject.value.count).to  eql 1
     expect(subject.value.first['value']).to eql 1000.0
   end
 end
