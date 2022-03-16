aa9a14107ca22f078db75b240b1ca65551f869a3
iamnader <iamnader@gmail.com>
2022-03-16 16:30:57 +0000

    Github Graphql Client

---
+++ server/app/lib/github_graphql.rb
@@ -0,0 +1,65 @@
+class GithubGraphql
+  API_URL = 'https://api.github.com/graphql'
+  DEFAULT_START_DATE = Date.new(2017, 1, 1)
+
+  def initialize
+    @headers = { 'Content-Type' => 'application/json',
+                 "Authorization": "Bearer #{ENV['GITHUB_TOKEN']}" }
+  end
+
+  def whoami
+    query = '
+      query {
+        viewer {
+          login
+        }
+      }
+    '
+
+    body = {
+      query: query
+    }.to_json
+
+    response = run_query(body)
+    response['data']['viewer']['login']
+  end
+
+  def dev_activity(repo:, day:)
+    start_time = day.to_time.utc.iso8601
+    end_time = (day + 1.day - 1.minute).to_time.utc.iso8601
+    query = '
+query ($user: String!, $name: String!, $startDate: GitTimestamp!, $endDate: GitTimestamp!) {
+  repository(owner: $user, name: $name) {
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(since: $startDate, until: $endDate) {
+            totalCount
+          }
+        }
+      }
+    }
+  }
+}
+    '
+
+    body = {
+      query: query,
+      variables: { user: repo.user, name: repo.name, startDate: start_time, endDate: end_time }
+    }.to_json
+
+    response = run_query(body)
+    response['data']['repository']['defaultBranchRef']['target']['history']['totalCount']
+  end
+
+  private
+
+  def run_query(body)
+    HTTParty.post(
+      API_URL,
+      headers: @headers,
+      body: body,
+      verify: false
+    ).parsed_response
+  end
+end
