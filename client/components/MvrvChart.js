16574fb5421123dc1a45bbce840a7482092b8580
iamnader <iamnader@gmail.com>
2022-03-17 14:22:05 +0000

    linear gradient

--- client/components/MvrvChart.js
+++ client/components/MvrvChart.js
@@ -1,11 +1,13 @@
 import {
   LineChart,
   Line,
+  Area,
   CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   ResponsiveContainer,
+  ComposedChart,
   Legend,
   ReferenceLine,
 } from 'recharts';
@@ -18,11 +20,16 @@ import {
   dateFormatter,
   mergeTimestamps,
 } from '../lib/formatters';
+import BtcArea from './BtcArea';
 
 export default function MvrvChart({ mvrv, btc }) {
   const theme = useTheme();
 
   const data = mergeTimestamps(mvrv, btc, 'btc');
+
+  const dataMax = Math.max(...data.map(i => i.v));
+  const endOfRed = (dataMax - 3) / dataMax
+  const startOfGreen = 100 - (1 / dataMax)
 
   return (
     <DashboardItem
@@ -30,26 +37,31 @@ export default function MvrvChart({ mvrv, btc }) {
       helpText="MVRV (market-value-to-realized-value) is a ratio of an asset's Market Capitalization versus its Realized Capitalization. By comparing these two metrics, MVRV can be used to get a sense of when price is above or below 'fair value', and to assess market profitability. Extreme deviations between market value and realized value can be used to identify market tops and bottoms as they reflect periods of extremes investor unrealized profit and loss, respectively"
     >
       <ResponsiveContainer width="99%" height={300}>
-        <LineChart
+        <ComposedChart
           data={data}
           margin={{ top: 5, right: 15, bottom: 5, left: 0 }}
         >
+          <defs>
+            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="100%">
+              <stop offset="0%" stopColor="red" stopOpacity={1} />
+              <stop offset={`${endOfRed}%`} stopColor="red" stopOpacity={1} />
+              <stop offset="70%" stopColor="gray" stopOpacity={1} />
+              <stop offset={`${startOfGreen}%`} stopColor="green" stopOpacity={1} />
+              <stop offset="100%" stopColor="green" stopOpacity={1} />
+            </linearGradient>
+          </defs>
           <Line
             type="monotone"
             dataKey="v"
             name="MVRV"
-            stroke={theme.palette.secondary.main}
+            stroke='url(#splitColor)'
+            // stroke={theme.palette.secondary.main}
+            // fill="green"
+            // fill="url(#splitColor)"
             dot={false}
             yAxisId="mvrv"
           />
-          <Line
-            type="monotone"
-            dataKey="btc"
-            name="BTC Price"
-            stroke={theme.palette.primary.main}
-            yAxisId="btc"
-            dot={false}
-          />
+          {BtcArea({})}
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis
             dataKey="ts"
@@ -68,19 +80,19 @@ export default function MvrvChart({ mvrv, btc }) {
           <ReferenceLine
             y={3}
             yAxisId="mvrv"
-            stroke="#B6465F"
+            stroke="red"
             strokeDasharray="2 2"
           />
           <ReferenceLine
             y={1}
             yAxisId="mvrv"
-            stroke="#678D58"
+            stroke="green"
             strokeDasharray="2 2"
           />
 
           <Tooltip labelFormatter={dateFormatter} />
           <Legend />
-        </LineChart>
+        </ComposedChart>
       </ResponsiveContainer>
     </DashboardItem>
   );
--- client/components/RhodlRatioChart.js
+++ client/components/RhodlRatioChart.js
@@ -1,5 +1,7 @@
 import {
   LineChart,
+  Area,
+  ComposedChart,
   Line,
   CartesianGrid,
   XAxis,
@@ -7,6 +9,7 @@ import {
   Tooltip,
   ResponsiveContainer,
   Legend,
+  ReferenceLine,
 } from 'recharts';
 import DashboardItem from './DashboardItem';
 import { useTheme } from '@mui/material';
@@ -16,11 +19,17 @@ import {
   dateFormatter,
   mergeTimestamps,
 } from '../lib/formatters';
+import BtcArea from './BtcArea';
 
 export default function RhodlRatioChart({ rhodlRatio, btc }) {
   const theme = useTheme();
 
   const data = mergeTimestamps(rhodlRatio, btc, 'btc');
+
+  const dataMax = Math.max(...data.map(i => i.v));
+  const dataMin = Math.min(...data.map(i => i.v));
+  const endOfRed = (dataMax - 50000) / dataMax;
+  const startOfGreen = 100 - 350 / dataMax;
 
   return (
     <DashboardItem
@@ -28,26 +37,34 @@ export default function RhodlRatioChart({ rhodlRatio, btc }) {
       helpText="The RHODL Ratio takes the ratio between the 1 week and the 1-2 years RCap HODL bands. In addition, it accounts for increased supply by weighting the ratio by the total market age. A high ratio is an indication of an overheated market and can be used to time cycle tops."
     >
       <ResponsiveContainer width="99%" height={300}>
-        <LineChart
+        <ComposedChart
           data={data}
           margin={{ top: 5, right: 15, bottom: 5, left: 10 }}
         >
+          <defs>
+            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="100%">
+              <stop offset="0%" stopColor="red" stopOpacity={0.5} />
+              <stop offset={`${endOfRed}%`} stopColor="gray" stopOpacity={1} />
+              {/* <stop offset="70%" stopColor="gray" stopOpacity={1} /> */}
+              <stop
+                offset={`${startOfGreen}%`}
+                stopColor="gray"
+                stopOpacity={1}
+              />
+              <stop offset="100%" stopColor="green" stopOpacity={1} />
+            </linearGradient>
+          </defs>
           <Line
             type="monotone"
             dataKey="v"
             name="RHODL Ratio"
-            stroke={theme.palette.secondary.main}
+            stroke="url(#splitColor)"
+            // stroke={theme.palette.secondary.main}
             dot={false}
             yAxisId="rhodlRatio"
           />
-          <Line
-            type="monotone"
-            dataKey="btc"
-            name="BTC Price"
-            stroke={theme.palette.primary.main}
-            yAxisId="btc"
-            dot={false}
-          />
+          {BtcArea({})}
+      
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis
             dataKey="ts"
@@ -58,6 +75,8 @@ export default function RhodlRatioChart({ rhodlRatio, btc }) {
           />
           <YAxis
             yAxisId="rhodlRatio"
+            scale="log"
+            domain={['auto', 'auto']}
             tickFormatter={nFormatter}
             stroke={theme.palette.secondary.main}
           />
@@ -68,8 +87,20 @@ export default function RhodlRatioChart({ rhodlRatio, btc }) {
             stroke={theme.palette.primary.main}
           />
           <Tooltip labelFormatter={dateFormatter} />
+          <ReferenceLine
+            y={50000}
+            yAxisId="rhodlRatio"
+            stroke="red"
+            strokeDasharray="2 2"
+          />
+          <ReferenceLine
+            y={350}
+            yAxisId="rhodlRatio"
+            stroke="green"
+            strokeDasharray="2 2"
+          />
           <Legend />
-        </LineChart>
+        </ComposedChart>
       </ResponsiveContainer>
     </DashboardItem>
   );
