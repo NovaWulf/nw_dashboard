878b6c980dcbbebe5df259ff41b879a22caa11ac
iamnader <iamnader@gmail.com>
2022-03-21 12:28:36 +0000

    another migration fix

--- server/db/migrate/20220321110336_unique_repo_commits.rb
+++ server/db/migrate/20220321110336_unique_repo_commits.rb
@@ -2,6 +2,10 @@ class UniqueRepoCommits < ActiveRecord::Migration[6.1]
   def change
     # some corrupt data
     RepoCommit.where('day > ?', 3.weeks.ago).delete_all
+    # remove dupes
+    columns_that_make_record_distinct = %i[repo_id day]
+    distinct_records = RepoCommit.select('MIN(id) as id').group(columns_that_make_record_distinct)
+    RepoCommit.where.not(id: distinct_records).destroy_all
     add_index :repo_commits, %i[repo_id day], unique: true
   end
 end
