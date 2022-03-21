f7e382fd8a76125e40084eb9fe958b947504e61f
iamnader <iamnader@gmail.com>
2022-03-21 13:09:20 +0000

    fake change

--- server/db/migrate/20220321110336_unique_repo_commits.rb
+++ server/db/migrate/20220321110336_unique_repo_commits.rb
@@ -1,5 +1,9 @@
 class UniqueRepoCommits < ActiveRecord::Migration[6.1]
   def change
+    # remove dupes
+    # columns_that_make_record_distinct = %i[repo_id day]
+    # distinct_records = RepoCommit.select('MIN(id) as id').group(columns_that_make_record_distinct)
+    # RepoCommit.where.not(id: distinct_records).delete_all
     add_index :repo_commits, %i[repo_id day], unique: true
   end
 end
