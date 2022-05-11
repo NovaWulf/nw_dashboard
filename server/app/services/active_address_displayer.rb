c294356a30d1167b77f5befc048fa06c6043a255
iamnader <iamnader@gmail.com>
2022-05-11 10:51:15 +0100

    change active addressses to daily metric

--- server/app/services/active_address_displayer.rb
+++ server/app/services/active_address_displayer.rb
@@ -6,10 +6,12 @@ class ActiveAddressDisplayer < BaseService
   end
 
   def run
-    # we group because the messari data comes in on wednesdays
-    Metric.by_token(token).by_metric('active_addresses').group_by_week(:timestamp, time_zone: false,
-                                                                                   week_start: :sunday).sum(:value).to_a.map do |m|
-      OpenStruct.new(timestamp: m[0], value: m[1])
-    end
+    Metric.by_token(token).by_metric('active_addresses').sundays.oldest_first
+
+    # # we group because the messari data comes in on wednesdays
+    # Metric.by_token(token).by_metric('active_addresses').group_by_week(:timestamp, time_zone: false,
+    #                                                                                week_start: :sunday).sum(:value).to_a.map do |m|
+    #   OpenStruct.new(timestamp: m[0], value: m[1])
+    # end
   end
 end
