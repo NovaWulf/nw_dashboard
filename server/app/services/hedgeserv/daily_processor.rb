3109e3cc7bfe38f3912665d45c81a9d96b2e9555
iamnader <iamnader@gmail.com>
2022-05-12 14:57:19 +0100

    fix empty email

--- server/app/services/hedgeserv/daily_processor.rb
+++ server/app/services/hedgeserv/daily_processor.rb
@@ -14,7 +14,7 @@ module Hedgeserv
       trades_text = csv_transactions_text ? trade_parser.run(csv_text: csv_transactions_text).value : nil
       positions_text = csv_positions_text ? positions_parser.run(csv_text: csv_positions_text).value : nil
 
-      return if trade_text.blank? && positions_text.blank?
+      return if trades_text.blank? && positions_text.blank?
 
       email_notification(trades_text, positions_text)
     end
