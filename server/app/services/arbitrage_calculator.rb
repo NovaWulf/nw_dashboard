85922ef4e1a2de8cbb7fe272b8534e79c5c6dcef
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-12 14:31:43 -0400

    pulling model weights in from table that is updated from r

--- server/app/services/arbitrage_calculator.rb
+++ server/app/services/arbitrage_calculator.rb
@@ -1,27 +1,26 @@
 class ArbitrageCalculator < BaseService
     
-    ETH_WEIGHT = 1
-    OP_WEIGHT = -2174.7378
-    CONST_WEIGHT = 37.531326
-    MODEL_ID = "adb21bd1-2204-ba24-07ac-36b78eb7dfc4"
 
     def run
     #   tracked_pairs = ["eth-usd","op-usd"]
     #   tracked_pairs.each do |p|
     #     Fetchers::CoinbaseFetcher.run(resolution: 60, pair: p)
     #   end
-    mostRecentModelID = CointegrationModels.newest_first.first&.uuid
-    puts "mostRecentModelID: " + mostRecentModelID.to_s
-    #   res= 60
-    #   last_timestamp = ModeledSignal.by_model(MODEL_ID).last&.starttime
+      mostRecentModelID = CointegrationModel.newest_first.first&.uuid
+      opWeight = CointegrationModelWeight.where("uuid = '#{mostRecentModelID}' and asset_name = 'op-usd'").pluck(:weight)[0]
+      ethWeight = CointegrationModelWeight.where("uuid = '#{mostRecentModelID}' and asset_name = 'eth-usd'").pluck(:weight)[0]
+      constWeight = CointegrationModelWeight.where("uuid = '#{mostRecentModelID}' and asset_name = 'det'").pluck(:weight)[0]
+      puts "OP weight: " + opWeight.to_s + " ETH_WEIGHT: " + ethWeight.to_s + " CONST_WEIGHT: " + constWeight.to_s
+      res= 60
+      last_timestamp = ModeledSignal.by_model(mostRecentModelID).last&.starttime
       
-    #   start_time = last_timestamp ? last_timestamp + res : Date.new(2022, 6, 13).to_time.to_i
-    #   puts "start time: " + start_time.to_s
+      start_time = last_timestamp ? last_timestamp + res : Date.new(2022, 6, 13).to_time.to_i
+      puts "start time: " + start_time.to_s
 
-    #   starttimes = Candle.by_resolution(res).where("starttime> " + start_time.to_s).pluck(:starttime)
-    #   puts "length of start times: " + starttimes.length().to_s
-    #   starttimes = starttimes.uniq.sort
-    #   puts "length of uniq start times: " + starttimes.length().to_s
+      starttimes = Candle.by_resolution(res).where("starttime> " + start_time.to_s).pluck(:starttime)
+      puts "length of start times: " + starttimes.length().to_s
+      starttimes = starttimes.uniq.sort
+      puts "length of uniq start times: " + starttimes.length().to_s
 
     #   counter=0
     #   starttimes.each do |time|
