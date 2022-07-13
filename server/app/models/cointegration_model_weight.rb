8f43927522a9db07546ed7494c1fa6489e378023
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-13 00:16:06 -0400

    adding model for cointegration model weight

---
+++ server/app/models/cointegration_model_weight.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: cointegration_model_weights
+#
+#  id         :bigint           not null, primary key
+#  asset_name :string
+#  timestamp  :integer
+#  uuid       :string
+#  weight     :float
+#
+class CointegrationModelWeight < ApplicationRecord
+    belongs_to :cointegration_model
+    #scope :by_model, ->(uuid) { joins(:cointegration_model).where(cointegration_models: {uuid: uuid})}
+  end
