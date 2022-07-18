103ece5941ced3bb39a673df512d5e7688286279
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-18 10:35:59 -0400

    added fetcher test

---
+++ server/spec/services/fetchers/coinbase_fetcher_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Fetchers::CoinbaseFetcher do
+    subject { described_class.run(resolution: 60, pair: "op-usd") }
+    include_context 'coinbase client'
+  
+    it 'persists' do
+      subject
+      expect(Candle.count).to eql 1
+      m = Candle.first
+      expect(m.volume).to eql 100.0
+      expect(m.open).to eql 100.0
+      expect(m.high).to eql 100.0
+      expect(m.close).to eql 100.0
+      expect(m.low).to eql 100.0
+      expect(m.starttime).to eql Time.now.to_i
+      expect(m.pair).to eql "op-usd"
+      expect(m.resolution).to eql 60
+      expect(m.exchange).to eql "Coinbase"
+    end
+  end
+  
