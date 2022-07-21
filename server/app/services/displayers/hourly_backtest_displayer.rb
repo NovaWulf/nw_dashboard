ce8882b33029b4a980c8cef9e8a4e3f790899d33
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-21 18:29:37 -0400

    re-added arbsignalchart

---
+++ server/app/services/displayers/hourly_backtest_displayer.rb
@@ -0,0 +1,18 @@
+module Displayers
+  class HourlyBacktestDisplayer < BaseService
+    attr_reader :model 
+
+    def initialize(model:)
+      if !model
+        Rails.logger.info "using default model for displayer -- latest"
+        model = CointegrationModel.newest_first.first&.uuid
+      end
+      @model = model
+    end
+
+    def run
+      ModeledSignal.by_model(@model+"-b").on_the_hour.oldest_first
+    end
+
+  end
+end
