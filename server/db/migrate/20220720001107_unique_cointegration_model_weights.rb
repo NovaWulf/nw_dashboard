02e5cc8e460c27a71661f23f38e5a9a02ddc58c0
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-21 18:34:14 -0400

    fixed unique index on cointegration model weights

--- server/db/migrate/20220720001107_unique_cointegration_model_weights.rb
+++ server/db/migrate/20220720001107_unique_cointegration_model_weights.rb
@@ -1,5 +1,5 @@
 class UniqueCointegrationModelWeights < ActiveRecord::Migration[6.1]
   def change
-    add_index :cointegration_model_weights, %i[uuid asset_name], unique: true
+    add_column :cointegration_model_weights,  %i[uuid asset_anem], unique: true
   end
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2022_07_21_004750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "backtests", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "starttime", null: false
-    t.string "model_id", null: false
-    t.boolean "in_sample", null: false
-    t.float "pnl", null: false
-  end
-
   create_table "candles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
