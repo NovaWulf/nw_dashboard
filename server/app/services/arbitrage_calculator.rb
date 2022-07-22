49bcdba5e853173ea74bff5fa27fdd866de59e40
iamnader <iamnader@gmail.com>
2022-07-22 14:25:07 +0100

    linting

--- server/app/services/arbitrage_calculator.rb
+++ server/app/services/arbitrage_calculator.rb
@@ -1,5 +1,4 @@
 class ArbitrageCalculator < BaseService
-
   def run
     fetch_coinbase_data
     most_recent_model_id = CointegrationModel.newest_first.first&.uuid
@@ -7,7 +6,7 @@ class ArbitrageCalculator < BaseService
     op_weight = CointegrationModelWeight.where("uuid = '#{most_recent_model_id}' and asset_name = 'op-usd'").pluck(:weight)[0]
     eth_weight = CointegrationModelWeight.where("uuid = '#{most_recent_model_id}' and asset_name = 'eth-usd'").pluck(:weight)[0]
     const_weight = CointegrationModelWeight.where("uuid = '#{most_recent_model_id}' and asset_name = 'det'").pluck(:weight)[0]
-    res= 60
+    res = 60
     last_timestamp = ModeledSignal.by_model(most_recent_model_id).last&.starttime
 
     return if last_timestamp && last_timestamp > Time.now.to_i - res
@@ -16,79 +15,73 @@ class ArbitrageCalculator < BaseService
 
     starttimes = Candle.by_resolution(res).where("starttime>= #{start_time}").pluck(:starttime)
     starttimes = starttimes.uniq.sort
-    
-    eth_not_null=0
-    op_not_null=0
+
+    eth_not_null = 0
+    op_not_null = 0
     index = 0
     current_eth_val = nil
     current_op_val = nil
-    #this code block cycles through the candles until we have a record from each of OP and ETH
-    #so that we can start the forward-filling of missing timesteps using most recent values
+    # this code block cycles through the candles until we have a record from each of OP and ETHHey
+    # so that we can start the forward-filling of missing timesteps using most recent values
     while true
       this_start_time = starttimes[index]
-      this_op_candle = Candle.by_resolution(res).by_pair("op-usd").where("starttime = #{this_start_time}")
-      this_eth_candle = Candle.by_resolution(res).by_pair("eth-usd").where("starttime = #{this_start_time}")
-      
-      if this_op_candle.count >0
-          op_not_null=1
-          current_op_val = this_op_candle.pluck(:close)[0]
+      this_op_candle = Candle.by_resolution(res).by_pair('op-usd').where("starttime = #{this_start_time}")
+      this_eth_candle = Candle.by_resolution(res).by_pair('eth-usd').where("starttime = #{this_start_time}")
+
+      if this_op_candle.count > 0
+        op_not_null = 1
+        current_op_val = this_op_candle.pluck(:close)[0]
       end
-      if this_eth_candle.count>0
-          eth_not_null=1
-          current_eth_val = this_eth_candle.pluck(:close)[0]
+      if this_eth_candle.count > 0
+        eth_not_null = 1
+        current_eth_val = this_eth_candle.pluck(:close)[0]
       end
-      if eth_not_null==1 and op_not_null==1
-        break
-      end
-      index = index+1
+      break if eth_not_null == 1 and op_not_null == 1
+
+      index += 1
     end
-    counter=0
-    length_start_times = starttimes.length()
-    starttimes = starttimes[index..(length_start_times-1)]
-    m=nil
+    counter = 0
+    length_start_times = starttimes.length
+    starttimes = starttimes[index..(length_start_times - 1)]
+    m = nil
     starttimes.each do |time|
-      this_op_candle = Candle.by_resolution(res).by_pair("op-usd").where("starttime = #{time}")
-      this_eth_candle = Candle.by_resolution(res).by_pair("eth-usd").where("starttime = #{time}")
-      
+      this_op_candle = Candle.by_resolution(res).by_pair('op-usd').where("starttime = #{time}")
+      this_eth_candle = Candle.by_resolution(res).by_pair('eth-usd').where("starttime = #{time}")
+
       # update current candle value if not null, otherwise, use most recent non-null value (flat-forward interpolation)
-      
-      if this_op_candle.count >0
-          current_op_val = this_op_candle.pluck(:close)[0]
-      end
-      if this_eth_candle.count>0
-          current_eth_val = this_eth_candle.pluck(:close)[0]
-      end
-      signal_value = current_op_val*op_weight + current_eth_val*eth_weight + const_weight
-      
-      m=ModeledSignal.create(starttime: time, model_id: most_recent_model_id, resolution: res, value: signal_value)
+
+      current_op_val = this_op_candle.pluck(:close)[0] if this_op_candle.count > 0
+      current_eth_val = this_eth_candle.pluck(:close)[0] if this_eth_candle.count > 0
+      signal_value = current_op_val * op_weight + current_eth_val * eth_weight + const_weight
+
+      m = ModeledSignal.create(starttime: time, model_id: most_recent_model_id, resolution: res, value: signal_value)
     end
 
-  email_notification(m) if m
+    email_notification(m) if m
   end
-  
 
-  def email_notification(arb_signal,sigma = 1)
-    most_recent_model =   CointegrationModel.newest_first.first
+  def email_notification(arb_signal, sigma = 1)
+    most_recent_model = CointegrationModel.newest_first.first
     in_sample_mean = most_recent_model&.in_sample_mean
-    in_sample_sd  = most_recent_model&.in_sample_sd 
+    in_sample_sd = most_recent_model&.in_sample_sd
     return unless arb_signal && most_recent_model
+
     signal_value = arb_signal.value
-    upper = in_sample_mean + sigma*in_sample_sd
-    lower = in_sample_mean - sigma*in_sample_sd
-    if signal_value > upper 
+    upper = in_sample_mean + sigma * in_sample_sd
+    lower = in_sample_mean - sigma * in_sample_sd
+    if signal_value > upper
       NotificationMailer.with(subject: 'Statistical Arbitrage Indicator Alert',
-                              text: "OP-ETH spread value (#{signal_value.round(2)}) is above the high band of Paul's indicator (#{(upper).round(2)}). Recommend buying OP and shorting ETH").notification.deliver_now
+                              text: "OP-ETH spread value (#{signal_value.round(2)}) is above the high band of Paul's indicator (#{upper.round(2)}). Recommend buying OP and shorting ETH").notification.deliver_now
     elsif signal_value < lower
       NotificationMailer.with(subject: 'Statistical Arbitrage Indicator Alert',
-                              text: "OP-ETH (#{signal_value.round(2)}) is below the low band of Paul's indicator (#{(lower).round(2)}). Recommend buying ETH and shorting OP").notification.deliver_now
+                              text: "OP-ETH (#{signal_value.round(2)}) is below the low band of Paul's indicator (#{lower.round(2)}). Recommend buying ETH and shorting OP").notification.deliver_now
     end
+  end
 
-  end
-    def fetch_coinbase_data 
-      tracked_pairs = ["eth-usd","op-usd"]
-      tracked_pairs.each do |p|
-        Fetchers::CoinbaseFetcher.run(resolution: 60, pair: p)
-      end
+  def fetch_coinbase_data
+    tracked_pairs = %w[eth-usd op-usd]
+    tracked_pairs.each do |p|
+      Fetchers::CoinbaseFetcher.run(resolution: 60, pair: p)
     end
   end
-  
+end
--- server/app/services/fetchers/coinbase_fetcher.rb
+++ server/app/services/fetchers/coinbase_fetcher.rb
@@ -1,36 +1,36 @@
 module Fetchers
-    class CoinbaseFetcher < BaseService
+  class CoinbaseFetcher < BaseService
     attr_reader :resolution, :pair
-  
+
     def initialize(resolution:, pair:)
       @resolution = resolution
       @pair = pair
     end
-  
+
     def run
-      last_timestamp = Candle.by_exchange("Coinbase").by_pair(pair).by_resolution(resolution).oldest_first.last&.starttime
+      last_timestamp = Candle.by_exchange('Coinbase').by_pair(pair).by_resolution(resolution).oldest_first.last&.starttime
 
       Rails.logger.info "last timestamp: #{last_timestamp}. downloading candles starting with this + #{resolution} seconds"
 
-      if last_timestamp &&  Time.now.to_i-last_timestamp<=resolution
-        Rails.logger.info "have recent data, returning"
+      if last_timestamp && Time.now.to_i - last_timestamp <= resolution
+        Rails.logger.info 'have recent data, returning'
         return
       end
       start_timestamp = last_timestamp ? last_timestamp + resolution : nil
       puts "fetching #{pair}, start date: #{start_timestamp}"
       Rails.logger.info "fetching #{pair}, start date: #{start_timestamp}"
-      response = coinbase_client.get_prices(pair: pair, start_time: start_timestamp, resolution:resolution )
+      response = coinbase_client.get_prices(pair: pair, start_time: start_timestamp, resolution: resolution)
       return if response.blank?
-      
+
       response.each do |m|
         Rails.logger.info "start time from coinbase: #{m[0]}"
-        Candle.create(starttime: m[0], pair: pair,exchange: "Coinbase",resolution: resolution,low: m[1],high: m[2],open: m[3],close: m[4],volume: m[5] )
+        Candle.create(starttime: m[0], pair: pair, exchange: 'Coinbase', resolution: resolution, low: m[1], high: m[2],
+                      open: m[3], close: m[4], volume: m[5])
       end
     end
+
     def coinbase_client
       Coinbase.new
     end
-    
-
   end
-  end
+end
