dbf93a007650dde66a8c1b7be4a75c676d6fed60
iamnader <iamnader@gmail.com>
2022-07-22 14:25:21 +0100

    add repos to install section

--- server/app/lib/r_adapter.rb
+++ server/app/lib/r_adapter.rb
@@ -1,48 +1,37 @@
-require "rinruby"
+require 'rinruby'
 
 class RAdapter
-    @env_name
-    def initialize
-        env_name = "test" 
-        if Rails.env.development?
-            env_name = "development"
-        elsif Rails.env.production?
-            env_name = "production"
-        end
-        @env_name = env_name
-        R.eval <<-DOC
+  def initialize
+    R.eval <<-DOC
             my_packages<-c("data.table","RODBC","urca","lubridate","digest","lattice","latticeExtra")
 
             install_if_missing = function(p) {
-            if (p %in% rownames(installed.packages()) == FALSE) {
-                install.packages(p)
-            }
+              if (p %in% rownames(installed.packages()) == FALSE) {
+                  install.packages(p, repos='http://cran.us.r-project.org')
+              }
             }
 
             invisible(sapply(my_packages, install_if_missing))
-            
-            DOC
-    end
-    def run_r_script(script,returnVal)
 
-        R.eval <<-DOC
+    DOC
+  end
+
+  def run_r_script(script, return_val)
+    R.eval <<-DOC
         #{script}
-        DOC
-        return R.pull returnVal.to_s # Be sure to return the object assigned in R script
-        
-    end
-    def cointegration_analysis(startTimeString: , endTimeString:)
+    DOC
+    R.pull return_val.to_s # Be sure to return the object assigned in R script
+  end
 
-        R.eval <<-DOC 
-       
+  def cointegration_analysis(start_time_string:, end_time_string:)
+    R.eval <<-DOC
+
         source("./cointegrationAnalysis.R")
 
-        dbhandle <- odbcDriverConnect('driver=./psqlodbcw.so;database=nw_server_#{@env_name};trusted_connection=true;uid=nw_server')
-        
-        fitModel(dbhandle,#{startTimeString},#{endTimeString},ecdet_param = "const")
-        
-        DOC
+        dbhandle <- odbcDriverConnect('driver=./psqlodbcw.so;database=nw_server_#{Rails.env};trusted_connection=true;uid=nw_server')
 
-    end
+        fitModel(dbhandle,#{start_time_string},#{end_time_string},ecdet_param = "const")
+
+    DOC
+  end
 end
-
