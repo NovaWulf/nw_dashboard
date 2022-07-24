07f6730d4caeeb874ddda18311d2957d39149b83
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-23 21:49:20 -0400

    uncommented secured

--- server/app/controllers/graphql_controller.rb
+++ server/app/controllers/graphql_controller.rb
@@ -4,7 +4,7 @@ class GraphqlController < ApplicationController
   # but you'll have to authenticate your user separately
   protect_from_forgery with: :null_session
 
-  #include Secured
+  include Secured
 
   def execute
     variables = prepare_variables(params[:variables])
