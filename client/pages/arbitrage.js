c1e89a09a458d1ab6d38b53bf636ee86695da9a4
iamnader <iamnader@gmail.com>
2022-07-26 10:59:45 +0100

    fake change

--- client/pages/arbitrage.js
+++ client/pages/arbitrage.js
@@ -1,8 +1,9 @@
+import * as React from 'react';
+
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import ClientOnly from 'components/ClientOnly';
 import Layout from 'components/layouts/Layout';
 import ArbitrageCharts from 'components/protocols/ArbitrageCharts';
-import * as React from 'react';
 
 const Arbitrage = () => {
   return (
