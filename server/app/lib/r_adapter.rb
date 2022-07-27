c657cf99155aedfc7b5d6a54639d3b6e842b134e
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-27 13:54:44 -0400

    moved all r installations to init.R

--- server/app/lib/r_adapter.rb
+++ server/app/lib/r_adapter.rb
@@ -2,18 +2,6 @@ class RAdapter
   require 'rinruby'
   def initialize
     @R = RinRuby.new
-    @R.eval <<-EOF
-            my_packages<-c("data.table","RODBC","urca","lubridate","digest","lattice","latticeExtra")
-
-            install_if_missing = function(p) {
-              if (p %in% rownames(installed.packages()) == FALSE) {
-                  install.packages(p, repos='http://cran.us.r-project.org')
-              }
-            }
-
-            invisible(sapply(my_packages, install_if_missing))
-
-    EOF
   end
 
   def run_r_script(script, return_val)
