0548fdd71e860a4fc97a39bebfc810e88a3d8586
iamnader <iamnader@gmail.com>
2022-07-29 10:41:49 +0100

    Add a dashboard link to notification emails

--- server/app/models/backtest_model.rb
+++ server/app/models/backtest_model.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: backtest_models
+#
+#  id              :bigint           not null, primary key
+#  sequence_number :integer
+#  version         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  model_id        :string
+#
+# Indexes
+#
+#  index_backtest_models_on_version_and_sequence_number  (version,sequence_number) UNIQUE
+#
 class BacktestModel < ApplicationRecord
   scope :by_version, ->(v) { where(version: v) }
   scope :oldest_first, -> { order(version: :asc) }
