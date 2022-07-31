9a7307df5084817a3c3a1cc1d2ec78a5ce6defee
ProfessorChaos <paul.lintilhac@gmail.com>
2022-07-31 16:00:57 -0400

    more logging for csv writer

--- server/app/services/csv_writer.rb
+++ server/app/services/csv_writer.rb
@@ -2,13 +2,15 @@ class CsvWriter < BaseService
 
   def run
     file = "#{Rails.root}/public/data.csv"
+    Rails.logger.info "writing csv to #{file}"
     CSV.open( file, 'w' ) do |writer|
-      table = Candle.all;0 # ";0" stops output.  Change "User" to any model.
+      table = Candle.all; # ";0" stops output. 
       writer << table.first.attributes.map { |a,v| a }
       table.each do |s|
         writer << s.attributes.map { |a,v| v }
       end
     end
+    Rails.logger.info "done writing csv"
   end
 
 end
