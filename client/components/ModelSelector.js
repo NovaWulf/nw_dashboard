bb468471e6a91eee419ea6bee0111bcb47350c46
iamnader <iamnader@gmail.com>
2022-08-10 12:24:22 -0400

    linting

--- client/components/ModelSelector.js
+++ client/components/ModelSelector.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client';
 import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 const getModelsQuery = gql`
   query {
-        backtestModelInfo(version:1) {
-            version
-            sequenceNumber
-        }
+    backtestModelInfo(version: 1) {
+      version
+      sequenceNumber
     }
+  }
 `;
 
-    const { loading, error, data } = useQuery(getModelsQuery);
-    if (loading) return <p>Loading Query...</p>;
-    if (error){
-      console.log(error)
-      return <p>Error in Query...</p>;
-    } 
-    const maxSeqNum=data.backtestModelInfo[0].sequenceNumber
-    var modelSequence = []
-    for (let i = 0; i <= maxSeqNum; i++) {
-      modelSequence.push(i)
-    }    
-    console.log("maxSeqNum: "+maxSeqNum)
-        return(
-        );
-}
 const ModelSelector = ({ model, handleChange }) => {
+  const { loading, error, data } = useQuery(getModelsQuery);
+  if (loading) return <p>Loading Query...</p>;
+  if (error) {
+    console.log(error);
+    return <p>Error in Query...</p>;
+  }
+  const maxSeqNum = data.backtestModelInfo[0].sequenceNumber;
+  var modelSequence = [];
+  for (let i = 0; i <= maxSeqNum; i++) {
+    modelSequence.push(i);
+  }
+  console.log('maxSeqNum: ' + maxSeqNum);
+  return (
     <Box sx={{ width: 120 }}>
       <FormControl fullWidth>
         <InputLabel id="select-model">Model #</InputLabel>
@@ -43,5 +41,7 @@ const ModelSelector = ({ model, handleChange }) => {
         </Select>
       </FormControl>
     </Box>
+  );
+};
 
 export default ModelSelector;
