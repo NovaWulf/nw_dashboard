85c2b8a66327b20927254c414d90f16778242440
iamnader <iamnader@gmail.com>
2022-08-30 17:44:32 +0100

    add the winners to the email output

--- server/spec/mailers/previews/notification_mailer_preview.rb
+++ server/spec/mailers/previews/notification_mailer_preview.rb
@@ -13,6 +13,8 @@ class NotificationMailerPreview < ActionMailer::Preview
     trades = Hedgeserv::TradeParser.run(csv_text: trades_file).value
     positions_file = File.read(Rails.root.join('spec', 'fixtures', 'files', 'sample_positions2.csv'))
     positions = Hedgeserv::PositionsParser.run(csv_text: positions_file).value
-    NotificationMailer.with(run_date: Date.today, trades: trades, positions: positions).daily_trades
+    winners = Hedgeserv::WinnersParser.run(csv_text: positions_file).value
+
+    NotificationMailer.with(run_date: Date.today, trades: trades, positions: positions, winners: winners).daily_trades
   end
 end
