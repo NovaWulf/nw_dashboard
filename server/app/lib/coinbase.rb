4a9a73c279bc6a690f6a9f3f9aa683df78c5384f
ProfessorChaos <paul.lintilhac@gmail.com>
2022-09-08 00:46:11 -0400

    added specs for mailer through backtester

--- server/app/lib/coinbase.rb
+++ server/app/lib/coinbase.rb
@@ -45,7 +45,6 @@ class Coinbase
       responses = []
       new_start_time = time_now - 298 * resolution
       new_start_time = start_timestamp if new_start_time < start_timestamp
-      puts "headers: #{path}"
       responses.concat self.class.get(
         "#{path}?start=#{new_start_time}&end=#{time_now}&granularity=#{resolution}", headers: generate_headers(path)
       ).parsed_response
