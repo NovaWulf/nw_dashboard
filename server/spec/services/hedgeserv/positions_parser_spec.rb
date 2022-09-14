6ea44f7a19b8cff9ead382f82c5a10c3701c2696
iamnader <iamnader@gmail.com>
2022-09-14 11:15:06 +0100

    update spec files based on new sample file

--- server/spec/services/hedgeserv/positions_parser_spec.rb
+++ server/spec/services/hedgeserv/positions_parser_spec.rb
@@ -3,8 +3,9 @@ RSpec.describe Hedgeserv::PositionsParser do
   subject { described_class.run(csv_text: csv_text) }
   let(:result) { subject.value }
 
-  context 'file with transactions' do
-    let(:filename) { 'sample_positions2.csv' }
+  context 'file with positions' do
+    let(:filename) { 'sample_positions.csv' }
+
     it 'returns grouped by Strategy' do
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eql 6 # 5 strategies and totals
@@ -12,20 +13,20 @@ RSpec.describe Hedgeserv::PositionsParser do
 
     it 'shows total' do
       expect(result[0][0]).to include 'Total'
-      expect(result[0][3]).to include '(2,816)'
-      expect(result[0][3]).to include '-8.8%'
+      expect(result[0][3]).to include '(4,763)'
+      expect(result[0][3]).to include '-13.0%'
     end
 
     it 'groups data' do
       expect(result[1][0]).to include 'Liquid Digital Assets'
-      expect(result[1][3]).to include '(180)'
+      expect(result[1][3]).to include '(109)'
 
-      expect(result[3][0]).to include 'Long / Short Public Securities'
-      expect(result[3][1]).to include '102'
+      expect(result[2][0]).to include 'Long / Short Public Securities'
+      expect(result[2][1]).to include '127'
     end
 
     it 'shows percent of fund' do
-      expect(result[1][0]).to include '0.5%'
+      expect(result[1][0]).to include '2.7%'
     end
   end
 end
